0da6ae1be3926c41ee51bedc3417471d90260247
steph802 <steph802@gmail.com>
2018-12-17 11:55:09 -0500

    Update cs361python.py

--- cs361python.py
+++ cs361python.py
@@ -39,3 +39,81 @@ def firstNPrimes(n):
         primes.append(x)
       x += 1
     print(primes)
+
+    
+    
+ ########################################################
+#
+#  Question 7
+#
+########################################################
+
+#Function that prints all elements of a list
+def listElements():
+  myList = [1,2,3,4,5]
+  print(myList)
+
+#Function that prints all elements of a list in reverse
+def reverseElements():
+  myList = [1,2,3,4,5]
+  print(myList[::-1])
+
+#Function that returns number of elements in a list
+def listLenght():
+  myList = [1,2,3,4,5]
+  print(len(myList))
+    
+    
+    
+    
+ ########################################################
+#
+#  Question 8
+#
+########################################################    
+
+def listEntries():
+  a = [1,2,3,4,5]
+  b = a
+  b[1] = 8
+  c = a[:]
+  c[2] = 9
+  print(a)
+  print(b)
+  print(c)
+
+def set_first_elem_to_zero(l):
+  l[0] = 0
+  print (l)
+
+myList = [1,2,3,4,5]
+set_first_elem_to_zero(myList)
+
+
+
+
+
+ ########################################################
+#
+#  Question 9
+#
+########################################################   
+
+def mergeList(l):
+  merged = [x for y in l for x in y]
+  print (merged)
+
+myList = [[1,3], [5,6]]
+mergeList(myList)
+
+
+
+
+
+
+ ########################################################
+#
+#  Question 11
+#
+########################################################   
+
